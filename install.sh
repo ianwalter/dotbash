3d22b172ebbd9cbec145706d044f1de3aaedf2ce
Ian Walter <ianwalter@fastmail.com>
2016-02-18 17:00:39 -0500

    Fixing else if

--- install.sh
+++ install.sh
@@ -6,17 +6,18 @@ mkdir -p ~/.bash/variables
 # Copy variables files.
 cp variables/* ~/.bash/variables/
 
-
 if [[ $(uname) == 'Darwin' ]]; then
 
   #
   cat .bash_profile osx.bash_profile > ~/.bash_profile
 
-else if [[ $(uname) == 'Linux' ]]; then
+else
+  if [[ $(uname) == 'Linux' ]]; then
 
-  # Copy .bash_profile to current user's directory.
-  cp .bash_profile ~/.bash_profile
+    # Copy .bash_profile to current user's directory.
+    cp .bash_profile ~/.bash_profile
 
+  fi
 fi
 
 # Source .bash_profile so that commands are executed immediately.
